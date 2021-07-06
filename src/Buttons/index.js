c5e173d7b56e9e12827e08ecf51e197ae0bb4d36
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-06 15:37:56 +0200

    Buttons component and style.css added

---
+++ src/Buttons/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+
+const Buttons = () => (
+    <p>
+    <button className="button">Ukryj nagłówek</button>
+
+    <button className="button">Zmień kolor tła</button>
+</p>
+);
+
+export default Buttons; 
