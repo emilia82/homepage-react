16dbda48798e07e3b7fc58a95b406500d81f74d9
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-06 15:50:01 +0200

    Footer component imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation";
 import Buttons from "./Buttons";
 import Header from "./Header";
+import Footer from "./Footer";
 
 
 
@@ -113,17 +114,7 @@ function App() {
                     
                  </section>
                  </main> 
-            <footer className="footer">
-                © Strona przygotowana w ramach kursu Frontend Developer od podstaw z firmą
-                <a
-                    className="footer__link"
-                    target="_blank"
-                    title="Oficjalna strona kursu YouCode."
-                    rel="noreferrer noopener"
-                    href="https://youcode.pl/"
-                >YouCode</a>
-                (strona otworzy się w nowej karcie).
-            </footer>             
+            <Footer />            
         </body>
   );
 }
