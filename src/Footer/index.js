b3237b9b73e6a6422e68792df635dca0fd1c8808
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-06 15:49:31 +0200

    Footer component & style.css added

---
+++ src/Footer/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+const Footer = () => (
+    <footer className="footer">
+                © Strona przygotowana w ramach kursu Frontend Developer od podstaw z firmą
+                <a
+                    className="footer__link"
+                    target="_blank"
+                    title="Oficjalna strona kursu YouCode."
+                    rel="noreferrer noopener"
+                    href="https://youcode.pl/"
+                >YouCode</a>
+                (strona otworzy się w nowej karcie).
+            </footer> 
+);
+
+export default Footer; 
