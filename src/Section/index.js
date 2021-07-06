6c7c6c3f0e95518521b663389108ba5489dcba82
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-06 16:08:21 +0200

    Section componend & style.css added

---
+++ src/Section/index.js
@@ -0,0 +1,66 @@
+import "./style.css";
+
+const Section = () => (
+    <section>
+            <img className="section__image" src="https://i.postimg.cc/28PXb5rx/Emilia-Warakomska.jpg" alt="Emilia Warakomska"></img>
+            <h2 id="o-mnie" className="section__header">O Mnie</h2>
+            
+            <p className="section__paragraph">
+                Nazywam się Emilia Warakomska.
+                <span className="ciekawyElement">
+                    Jestem osobą o wielu talentach i wielu umiejętnościach.
+                </span>
+                Jestem naturalnym Liderem, pasjonuje mnie rozwój osobisty, zawodowy, duchowy i charakterologiczny.
+                Wykonałam wszystkie możliwe testy osobowości testy Tallentów, a ostatnio sie dowiedziałam, że
+                <br></br>
+                <strong>
+                    wg Wyroczni Delfickiej
+                </strong>
+                <q>Poznaj samego siebie - to klucz do sukcesu</q>
+                .
+                
+                I chyba coś w tym jest.
+            </p>
+            <hr></hr>
+            <p className="section__paragraph">
+                Uwielbiam pracę kreatywną, sport, muzykę, a także... filmy o superbohaterach, których tytuły przedstawię na tej stronie.
+               Od 16 lat moją pasją jest zdrowy styl życia, wszystko co związane z anty-aging, psychologia sportu, dieta, trening, suplementacja.
+               To moja zarówno pasja, jak i obsesja. Bardzo lubię też social-media, i wszelkie możliwości, które one dają
+            </p>
+        </section>
+        <section>
+            <h2 id="moje-sukcesy" className="section__header">Moje sukcesy</h2>
+            <p className="section__paragraph">
+                W 2016 roku zdobyłam uprawnienia do prowadzenia zajęć grupowych fitness i uzyskałam legitymację Ministra Sportu. 
+               Posiadam również tytuł International Personal Trainer. Posiadam również szkolenia z zakresu dietetyki sportowej, 
+               dietetyki klinicznej oraz suplementacji. Pod tym względem jestem Trenerem z krwi, kości i powołania.
+               Każdą drużynę ((i każdego człowieka) poprowadzę DO ZWYCIĘSTWA i pokażę drogę jak zdobyć każdy Everest życia.
+               W 2010 roku zaczęłam pisać bloga, napisałam kilka e-booków, zebrałam własną listę meilingową (5000 osób) i stworzyłam
+               po drodze kilka mikro-społeczności i gangów ("TRX Soldiers", "Strong & Sexy Girs").
+               Współpracowałam z portalami o tematyce zdrowia (poprostuzdrowo.pl) oraz pisałam artykuły do gazet ("Stuff", "Szaman").
+               Napisałam ponad 500 artykułów, ponad 600 wpisów na social media, stworzyłam kilkadziesiąt kreacji reklamowych.
+            </p>
+            <hr></hr>
+            <p className="section__paragraph">
+                <strong>Umiejętności i kompetencje:</strong>
+                zarządzanie, motywacja, techniki negocjacji, social-selling, prowadzenie mediów społecznościowych,
+               promotion, blogowanie, tworzenie pięknych prezentacji, prostych grafik, info-produktów, copywriting, posiadam prawo jazdy kat.B.
+               Porozumiewam się swobodnie po angielsku
+            </p>
+            </section>
+            <section>
+                <h2 id="ulubione-filmy" className="section__header">Ulubione filmy</h2>
+                <p className="section__paragraph">
+                    <u>Nie jestem "kinomaniakiem"</u>
+                    , wolę książki i mam ich całą bibliotekę. Ale jak już oglądam filmy, to zawsze mnie fascynowały te o bohaterach z super-MOC-ami :D 
+                bo sama zawsze chciałam takowe posiadać :D Więc o nich tutaj wspominam.
+                </p>
+                <p className="section__paragraph">
+                        Tak, poza tym... Lubię kino psychologiczne i takie, które daje do myślenia, albo zmienia perspektywę. 
+                    A jeśli chcę się pośmiać, to - chociaż w życiu moim nie brak śmiesznych sytuacji - zdecydowanie wolę kabarety :)
+                    </p>
+                    
+                 </section>
+);
+
+export default Section;
