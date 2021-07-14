f3a541434dc79ed78852e6b07deb988fe3c67de4
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-14 13:26:16 +0200

    subHeader added to component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,14 @@
 import "./style.css";
 
 const Header = ({ title, subTitle }) => (
+    <>
     <header>
     <h1>{title}</h1>
-    <h2>{subTitle}</h2>
-  </header>
+      </header>
+  <subHeader>
+<h2>{subTitle}</h2>
+  </subHeader>
+    </>
 );
 
 export default Header; 
